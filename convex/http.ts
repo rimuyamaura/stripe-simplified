c147b008be2c5c26afd6650e37d6dbdb43ae3d7c
rimuyamaura <148062753+rimuyamaura@users.noreply.github.com>
2024-11-08 17:04:45 +1300

    create user complete

--- convex/http.ts
+++ convex/http.ts
@@ -1,53 +1,65 @@
-// import { httpRouter } from "convex/server";
-// import { httpAction } from "./_generated/server";
-// import { Webhook } from 'svix'
-// import { WebhookEvent} from '@clerk/nextjs/server'
+import { httpRouter } from 'convex/server';
+import { httpAction } from './_generated/server';
+import { Webhook } from 'svix';
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { api } from './_generated/api';
 
-// const http = httpRouter();
+const http = httpRouter();
 
-// const clerkWebhook = httpAction(async (ctx, request) => {
-//     const webhookSecret = process.env.CLERK_WEBHOOK_SECRET
-//     if (!webhookSecret) {
-//         throw new Error('Missing CLERK_WEBHOOK_SECRET')
-//     }
+const clerkWebhook = httpAction(async (ctx, request) => {
+  const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+  if (!webhookSecret) {
+    throw new Error('Missing CLERK_WEBHOOK_SECRET environment variable');
+  }
 
-//     const svix_id = request.headers.get('svix-id')
-//     const svix_signature = request.headers.get('svix-signature')
-//     const svix_timestamp = request.headers.get('svix-timestamp')
-//     if (!svix_id || !svix_signature || !svix_timestamp) {
-//         return new Response('Error occurred -- Missing svix headers', {
-//             status: 400
-//         });
-//     }
+  const svix_id = request.headers.get('svix-id');
+  const svix_signature = request.headers.get('svix-signature');
+  const svix_timestamp = request.headers.get('svix-timestamp');
+  if (!svix_id || !svix_signature || !svix_timestamp) {
+    return new Response('Error occurred -- Missing svix headers', {
+      status: 400,
+    });
+  }
 
-//     const payload = await request.json();
-//     const body = JSON.stringify(payload);
+  const payload = await request.json();
+  const body = JSON.stringify(payload);
 
-//     const wh = new Webhook(webhookSecret)
-//     let evt: WebhookEvent
+  const wh = new Webhook(webhookSecret);
+  let evt: WebhookEvent;
 
-//     try {
-//         evt = wh.verify(body, {
-//             'svix-id': svix_id,
-//             'svix-signature': svix_signature,
-//             'svix-timestamp': svix_timestamp
-//         })
-//     } catch (error) {
-//         console.error('Error verifying webhook signature', error);
-//         return new Response('Error occurred', { status: 400 });
-//     }
+  try {
+    evt = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-signature': svix_signature,
+      'svix-timestamp': svix_timestamp,
+    }) as WebhookEvent;
+  } catch (error) {
+    console.error('Error verifying webhook signature', error);
+    return new Response('Error occurred', { status: 400 });
+  }
 
-//     try {
-//         // save user to db
-//     } catch (error) {
+  const eventType = evt.type;
 
-//     }
-// )
+  if (eventType == 'user.created') {
+    const { id, email_addresses, first_name, last_name } = evt.data;
+    const email = email_addresses[0]?.email_address;
+    const name = `${first_name || ''} ${last_name || ''}`;
 
-// http.route({
-//     path:'/clerk-webhook',
-//     method:'POST',
-//     handler: clerkWebhook
-// })
+    try {
+      await ctx.runMutation(api.users.createUser, { email, name, clerkId: id });
+    } catch (error) {
+      console.error('Error creating user in convex', error);
+      return new Response('Error creating user', { status: 500 });
+    }
+  }
+  return new Response('User created successfully', { status: 200 });
+});
 
-// export default http;
+// listener for incoming webhook events
+http.route({
+  path: '/clerk-webhook',
+  method: 'POST',
+  handler: clerkWebhook,
+});
+
+export default http;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,9 @@ import { useQuery } from 'convex/react';
 import { api } from '../../convex/_generated/api';
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.getAllTasks);
   return (
     <>
-      <h1>All tasks in db</h1>
-      {tasks?.map((task) => (
-        <div key={task.id}>
-          <h2>
-            {task.text} = is completed: {task.isCompleted ? 'true' : 'false'}
-          </h2>
-        </div>
-      ))}
+      <h1>Hello world</h1>
     </>
   );
 }
